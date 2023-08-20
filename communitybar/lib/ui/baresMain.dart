0eb10b05b7b49188209e265d23a77bea087326ce
DavidAlex99 <daalrive@espol.edu.ec>
2023-08-20 00:55:08 -0500

    establecimiento con la api. Falta diseno de la misma

---
+++ communitybar/lib/ui/baresMain.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class BaresMain extends StatefulWidget {
+  final String barName;
+
+  BaresMain({required this.barName});
+
+  @override
+  _BaresMainState createState() => _BaresMainState();
+}
+
+class _BaresMainState extends State<BaresMain> {
+  late Future<Bar> futureBar;
+
+  @override
+  void initState() {
+    super.initState();
+    futureBar = fetchBar(widget.barName);
+  }
+
+  Future<Bar> fetchBar(String barName) async {
+    final response =
+        await http.get(Uri.parse('http://10.0.2.2:3000/api/bares/$barName'));
+    if (response.statusCode == 200) {
+      return Bar.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Error al cargar los datos del bar.');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.barName),
+      ),
+      body: FutureBuilder<Bar>(
+        future: futureBar,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData) {
+              Bar bar = snapshot.data!;
+              // Aquí construyes tu widget usando la información de `bar`
+            } else if (snapshot.hasError) {
+              return Center(child: Text("${snapshot.error}"));
+            }
+          }
+          return CircularProgressIndicator();
+        },
+      ),
+    );
+  }
+}
+
+class Bar {
+  final String name;
+  final String ubication;
+  final List<Menu> menu;
+  final List<Poster> poster;
+
+  Bar({
+    required this.name,
+    required this.ubication,
+    required this.menu,
+    required this.poster,
+  });
+
+  factory Bar.fromJson(Map<String, dynamic> json) {
+    return Bar(
+      name: json['name'],
+      ubication: json['ubication'],
+      menu: (json['menu'] as List).map((i) => Menu.fromJson(i)).toList(),
+      poster: (json['poster'] as List).map((i) => Poster.fromJson(i)).toList(),
+    );
+  }
+}
+
+class Menu {
+  final String name;
+  final double price;
+
+  Menu({required this.name, required this.price});
+
+  factory Menu.fromJson(Map<String, dynamic> json) {
+    return Menu(
+      name: json['name'],
+      price: json['price'].toDouble(),
+    );
+  }
+}
+
+class Poster {
+  final String namePhoto;
+  final String url;
+
+  Poster({required this.namePhoto, required this.url});
+
+  factory Poster.fromJson(Map<String, dynamic> json) {
+    return Poster(
+      namePhoto: json['namePhoto'],
+      url: json['url'],
+    );
+  }
+}
--- communitybar/lib/ui/mainPage.dart
+++ communitybar/lib/ui/mainPage.dart
@@ -1,16 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:communitybar/ui/baresMain.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-//visualizar todos los eventos
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  late Future<List<Bar>> futureBars;
+
+  @override
+  void initState() {
+    super.initState();
+    futureBars = fetchBars();
+  }
+
+  Future<List<Bar>> fetchBars() async {
+    final response = await http.get(Uri.parse(
+        'http://10.0.2.2:3000/api/bares/')); // Asume que tienes un endpoint que devuelve todos los bares
+    if (response.statusCode == 200) {
+      List<dynamic> jsonResponse = json.decode(response.body);
+      return jsonResponse.map((bar) => Bar.fromJson(bar)).toList();
+    } else {
+      throw Exception('Error al cargar los bares.');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Pagina de visualizaciones'),
       ),
-      body: Center(
-        child: Text('Aqui estar las visualizacioes de los eventos'),
+      body: FutureBuilder<List<Bar>>(
+        future: futureBars,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData) {
+              List<Bar> bars = snapshot.data!;
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GridView.count(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 8,
+                  mainAxisSpacing: 8,
+                  children: bars.map((bar) {
+                    return _buildGridItem(bar.poster[0].url, bar.name, context);
+                  }).toList(),
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return Center(child: Text("${snapshot.error}"));
+            }
+          }
+          return Center(child: CircularProgressIndicator());
+        },
       ),
+    );
+  }
+
+  Widget _buildGridItem(String imageUrl, String barName, BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // Navega a BaresMain cuando se toca la imagen
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => BaresMain(barName: barName),
+          ),
+        );
+      },
+      child: Image.network(imageUrl, fit: BoxFit.cover),
     );
   }
 }
